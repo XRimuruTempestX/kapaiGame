8cb6c44eebcbf8fdb23154555123fd81620c6eb6
XRimuruTempestX <98110983+XRimuruTempestX@users.noreply.github.com>
2026-03-21 02:47:50 +0800

    luban

---
+++ LuBan/luban_examples-main/MiniTemplate/gen.bat
@@ -0,0 +1,12 @@
+set WORKSPACE=..
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=.
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -d json ^
+    -c cs-newtonsoft-json ^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputDataDir=output ^
+    -x outputCodeDir=Assets/Gen
+pause
